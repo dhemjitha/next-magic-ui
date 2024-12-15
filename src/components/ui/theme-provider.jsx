c59529a192bf2870d7116d157a2ef9c1c696c4b0
Dulran Hemjitha <dhemjitha@gmail.com>
2024-12-16 00:39:52 +0530

    feat: dark mode and retro grid

---
+++ src/components/ui/theme-provider.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+const ThemeProvider = ({ children, ...props }) => (
+  <NextThemesProvider {...props}>
+    {children}
+  </NextThemesProvider>
+);
+
+export default ThemeProvider;
