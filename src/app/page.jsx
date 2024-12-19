ee39db958d6020197e244216e0d1b9ff2269c6ae
Dulran Hemjitha <dhemjitha@gmail.com>
2024-12-19 21:22:24 +0530

    fixed: change background color

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg  bg-background md:shadow-xl">
+    <div className="bg-black relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg  bg-background md:shadow-xl">
       <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
         Retro Grid
       </span>
